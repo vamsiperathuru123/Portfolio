db9d1f5184873024613c661fea69ecd315b54010
PERATHURU CHANDRAVAMSI <vamsiperathuru@gmail.com>
2025-01-01 07:47:56 +0530

    changed contactpage

--- src/Contact.js
+++ src/Contact.js
@@ -4,67 +4,105 @@ import "./Contact.css";
 import "./About.css";
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import SimpleFooter from './SimpleFooter';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-   faLinkedin,
-   faFacebook,
-   faXTwitter,
-   faInstagram,
+  faLinkedin,
+  faFacebook,
+  faTwitter,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
-
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
-  const [text]=useTypewriter({
-    words:['Contact Details'],
-    loop:true,
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm('service_r04dyvt', 'template_l8rz1us', e.target, 'oU_A8_OHTnBT-h-I1')
+      .then(
+        (result) => {
+          e.target.reset();
+          alert("Successfully Sent");
+          console.log(result.txt);
+        },
+        (error) => {
+          alert("Error");
+          console.log(error);
+        }
+      );
+  };
+
+  const [text] = useTypewriter({
+    words: ['Contact Details'],
+    loop: true,
   });
+
   return (
     <div className='whole_contact'>
-        <div><Navbar/></div>
-        <div className='box'>
-          <h2>
-            {' '}
-            <span style={{ color: "red", fontWeight: "bold", fontFamily: "Times New Roman"   }}>{text}</span>
-            <span style={{ color: "red", fontFamily: "Times New Roman"   }}><Cursor/></span>
-          </h2>
-          <div className='contact'>
-            <div>
-              <div className='Address'>
-                <h3>Address :</h3>
-                <h4>H.N0 : 89/13, 1st Main Road, </h4>
-                <h4>RamaKrishna Nagar,</h4>
-                <h4>Ernavoor,</h4>
-                <h4>Tiruvallur-dist,</h4>
-                <h4>Chennai-600057,</h4>
-                <h4>Tamil Nadu.</h4>
-                <h4>Mobile No : 9840036307</h4>
-                <h4>Mail : vamsiperathuru@gmail.com</h4>
-              </div>
-              <div className='socialmedia'>
-                  <h3>Social Media Links :</h3>
-                  <div className='Button__links_c'>
-                              <a className="LinkedIn" href="https://www.linkedin.com/in/perathuru-chandravamsi-0b214021a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
-                                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                              </a>
-                              <a className="Instagram" href="https://www.instagram.com/chandravamsi.p?igsh=NTJlenlrcTc3ZDhh">
-                                  <FontAwesomeIcon icon={faInstagram} size="2x"  />
-                              </a>
-                              <a className="Facebook" href="https://www.facebook.com/perathuru.vamsi?mibextid=ZbWKwL">
-                                  <FontAwesomeIcon icon={faFacebook} size="2x"  />
-                              </a>
-                              <a className="X" href="https://x.com/ChandravamsiP?s=09">
-                                  <FontAwesomeIcon icon={faXTwitter} size="2x"  />
-                              </a>     
-                  </div>
+      <Navbar />
+      <div className='box'>
+        <h2>
+          <span style={{ color: "red", fontWeight: "bold", fontFamily: "Times New Roman" }}>{text}</span>
+          <Cursor />
+        </h2>
+        <div className='contact'>
+          <div className='details'>
+            <div className='Address'>
+              <h3>Address :</h3>
+              <h4>H.N0 : 89/13, 1st Main Road, </h4>
+              <h4>RamaKrishna Nagar,</h4>
+              <h4>Ernavoor,</h4>
+              <h4>Tiruvallur-dist,</h4>
+              <h4>Chennai-600057,</h4>
+              <h4>Tamil Nadu.</h4>
+              <h4>Mobile No : 9840036307</h4>
+              <h4>Mail : vamsiperathuru@gmail.com</h4>
+            </div>
+            <div className='socialmedia'>
+              <h3>Social Media Links :</h3>
+              <div className='Button__links_c'>
+                <a className="LinkedIn" href="https://www.linkedin.com/in/perathuru-chandravamsi-0b214021a">
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+                <a className="Instagram" href="https://www.instagram.com/chandravamsi.p">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a className="Facebook" href="https://www.facebook.com/perathuru.vamsi">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a className="Twitter" href="https://twitter.com/ChandravamsiP">
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
               </div>
             </div>
           </div>
-          <div>
-            <SimpleFooter/>
+          <div className='form_container'>
+            <div className='forms'>
+              <form onSubmit={sendEmail}>
+                <div className='form_com'>
+                  <label>Name</label>
+                  <input type='text' placeholder='Enter Name' required name='name' />
+                </div>
+                <div className='form_com'>
+                  <label>Email</label>
+                  <input type='email' placeholder='Enter Email' required name='Email' />
+                </div>
+                <div className='form_com'>
+                  <label>Contact No</label>
+                  <input type="tel" placeholder="Enter your mobile number" pattern="[0-9]{10}" maxLength="10" required name='contact' />
+                </div>
+                <div className='form_com'>
+                  <label>Message</label>
+                  <textarea placeholder='Write Your Message' name='message' required rows="4" cols="50" />
+                </div>
+                <div className='form_btn'>
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-
-    
+        <SimpleFooter />
+      </div>
     </div>
-  )
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -72,7 +72,7 @@ export default function Home() {
                     create dynamic, user-friendly interfaces. Passionate about leveraging technical skills in a collaborative team
                     environment to contribute to impactful projects and continue professional growth.
                     </h4>
-                    <a href="/Perathuru Chandravamsi - U3.pdf" download="Perathuru Chandravamsi - U3.pdf">
+                    <a href="/Perathuru Chandravamsi - U4.pdf" download="Perathuru Chandravamsi - U4.pdf">
                        <button>Download Resume</button>
                     </a>
                     <div className="Button__links">
